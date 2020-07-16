974489201775869992f44ae6e04b6403162ce652
Jose Morin <carlosmorin78@gmail.com>
2020-07-15 23:32:45 -0500

    Destroy reques spec

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -16,4 +16,42 @@ RSpec.describe "Categories", type: :request do
     end
   end
 
+  describe "POST api/v1/categories", :type => :request do
+    before do
+      post '/api/v1/categories', params: { category: { name: 'Perros' }}
+    end
+
+    it 'returns the catgeory name' do
+      expect(JSON.parse(response.body)['name']).to eq('Perros')
+    end
+
+    it 'returns a created status' do
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe "PUT /api/v1/categories/:id" do
+    before(:each) do
+      @category = create(:category)
+    end
+
+    it 'updates a category' do
+      put "/api/v1/categories/#{@category.id}", params: { category: {name: 'Gatos'} }
+
+      expect(response.status).to eq(200)
+      expect(Category.find(@category.id).name).to eql('Gatos')
+    end
+  end
+
+  describe "DELETE /api/v1/categories/:id" do
+    before do
+      @category = create(:category)
+    end
+
+    it 'returns a 204 status' do
+      delete "/api/v1/categories/#{@category.id}"
+
+      expect(response.status).to eq(204)
+    end
+  end
 end
