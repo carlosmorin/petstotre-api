9cd7511766a4aa5562294d4ae6f9cd7ef8a8453f
Jose Morin <carlosmorin78@gmail.com>
2020-07-15 22:02:34 -0500

    Rspec category controllers

---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,55 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::CategoriesController, type: :controller do
+  let(:valid_attributes) do
+    {
+      name: "Perros"
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      name: nil
+    }
+  end
+
+  describe "GET #index" do
+    it "returns a success response" do
+      Category.create! valid_attributes
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a success response" do
+      category = Category.create! valid_attributes
+      get :show, params: { id: category.id }
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new Category" do
+        expect {
+          post :create, params: {category: valid_attributes}
+        }.to change(Category, :count).by(1)
+      end
+
+      it "renders a JSON response with the new category" do
+        post :create, params: {category: valid_attributes}
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+
+    context "with invalid params" do
+      it "renders a JSON response with errors for the new category" do
+        post :create, params: {category: invalid_attributes}
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+  end
+end
