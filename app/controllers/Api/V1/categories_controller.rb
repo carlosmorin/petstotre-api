2657a8aae32d1467114041906f1b391698819a18
Jose Morin <carlosmorin78@gmail.com>
2020-07-15 21:06:01 -0500

    routes and model test

--- app/controllers/Api/V1/categories_controller.rb
+++ app/controllers/Api/V1/categories_controller.rb
@@ -1,5 +1,42 @@
-class Api::V1::CategoriesController < ApiController 
+class Api::V1::CategoriesController < ApiController
+  before_action :set_category, only: %i[show update destroy]
+
   def index
     @categories = Category.all
   end
+
+  def show
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      render json: @category, status: :created
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      render json: @category
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+  end
+
+  private
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
